a7ead92bab1f2e3884f6af56b5fc9cc9ccf55ae8
Aster808 <candanon7564@student.faytechcc.edu>
2024-09-19 10:32:16 -0400

    Add files via upload

---
+++ P1LAB1.py
@@ -0,0 +1,13 @@
+# Nicholas Candanoza
+# 9/17/24
+# P1LAB1
+# Get input from user and display output
+
+# Get first name from user
+first_name = input("Enter your first name: James")
+
+# Get last name from user
+Last_name = input("Enter your last name: Baxter ")
+
+# Display info to user
+print("hello,", first_name, last_name, + "! Welcome to CTI-110" )
